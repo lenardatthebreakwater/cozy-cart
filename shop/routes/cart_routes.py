fbd3b0625890c2e33f25e36d3fbbedebc625d189
Lenard <energeticbowlofrice@gmail.com>
2024-02-09 10:37:51 +0800

    Made some changes to cart_routes.py

--- shop/routes/cart_routes.py
+++ shop/routes/cart_routes.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, session, redirect, abort
+from flask import Blueprint, render_template, session, redirect, abort, url_for
 from shop.utils import paymongo_request
 from shop.forms import CheckoutForm
 from shop.models import PendingOrder
-from shop import fernet
+from shop import fernet, db
+import requests
 
 cart_blueprint = Blueprint("cart_blueprint", __name__)
 
@@ -10,27 +11,33 @@ cart_blueprint = Blueprint("cart_blueprint", __name__)
 def cart():
   form = CheckoutForm()
   if form.validate_on_submit():
-    """
-    encrypted_firstname = fernet.encrypt(form.firstname.data.encode()).decode()
-    encrypted_lastname = fernet.encrypt(form.lastname.data.encode()).decode()
-    encrypted_email = fernet.encrypt(form.email.data.encode()).decode()
-    encrypted_phone = fernet.encrypt(form.phone.data.encode()).decode()
-    customer_address = f"{form.line1.data}, {form.city.data}, {form.state.data}, {form.country.data}"
-    encrypted_address = fernet.encrypt(customer_address.encode()).decode()
-    encrypted_postal_code = fernet.encrypt(form.postal_code.data.encode()).decode()
-    """
-    """
-    new_order = PendingOrder(
-      firstname=encrypted_firstname,
-      lastname=encrypted_lastname,
-      email=encrypted_email,
-      phone=encrypted_phone,
-      address=encrypted_address,
-      postal_code=encrypted_postal_code,
-    )
-    db.session.add(new_order)
-    db.session.commit()
-    """
+    try:
+      encrypted_firstname = fernet.encrypt(form.firstname.data.encode()).decode()
+      encrypted_lastname = fernet.encrypt(form.lastname.data.encode()).decode()
+      encrypted_email = fernet.encrypt(form.email.data.encode()).decode()
+      encrypted_phone = fernet.encrypt(form.phone.data.encode()).decode()
+      customer_address = f"{form.line1.data}, {form.city.data}, {form.state.data}, {form.country.data}"
+      encrypted_address = fernet.encrypt(customer_address.encode()).decode()
+      encrypted_postal_code = fernet.encrypt(form.postal_code.data.encode()).decode()
+    except Exception as e:
+      print(e)
+      abort(500)
+
+    try:
+      new_order = PendingOrder(
+        firstname=encrypted_firstname,
+        lastname=encrypted_lastname,
+        email=encrypted_email,
+        phone=encrypted_phone,
+        address=encrypted_address,
+        postal_code=encrypted_postal_code
+      )
+      db.session.add(new_order)
+      db.session.commit()
+    except Exception as e:
+      print(e)
+      abort(500)
+
     try:
       line_items = []
       for cart_item in session["cart"]:
@@ -40,7 +47,9 @@ def cart():
           "name": cart_item["name"],
           "quantity": cart_item["quantity"]
         })
-    
+      
+      success_url = url_for("cart_blueprint.checkout_success")
+
       paymongo_checkout_id, paymongo_checkout_url = paymongo_request(
         line1=form.line1.data,
         city=form.city.data,
@@ -50,18 +59,33 @@ def cart():
         name=f"{form.firstname.data} {form.lastname.data}",
         email=form.email.data,
         phone=form.phone.data,
-        line_items=line_items,
+        line_items=line_items
       )
+
+      if "paymongo_checkout_id" not in session:
+        session["paymongo_checkout_id"] = fernet.encrypt(paymongo_checkout_id.encode()).decode()
 
       return redirect(paymongo_checkout_url)
     except Exception as e:
+      print(e)
       abort(500)
   
   return render_template("cart.html", title="Cart", form=form)
 
 
+"""
 @cart_blueprint.post("/cart/checkout/success")
 def checkout_success():
-  session.clear()
-  #Expire the paymongo checkout session object using the paymongo checkout session id stored in the session
-  return render_template("checkout_success.html")
+  try:
+    encrypted_paymongo_checkout_id = session["paymongo_checkout_id"]
+    paymongo_checkout_id = fernet.decrypt(encrypted_paymongo_checkout_id)
+    url = f"https://api.paymongo.com/v1/checkout_sessions/{paymongo_checkout_id}/expire"
+    headers = {"accept": "application/json"}
+    response = requests.post(url, headers=headers)
+    print(response.text)
+    session.clear()
+    return "<h1>You have successfully checked out</h1>"
+  except Exception as e:
+    print(e)
+    abort(500)
+"""
--- shop/static/script.js
+++ shop/static/script.js
@@ -5,19 +5,3 @@ const navBarMenu = document.querySelector("#navBarMenu");
 navBarBurger.addEventListener("click", () => {
 	navBarMenu.classList.toggle("is-active");
 })
-
-
-//for checkout form logic
-const checkoutForm = document.querySelector("#checkoutForm")
-const checkoutFormOpenBtn = document.querySelector("#checkoutFormOpenBtn")
-const checkoutFormCloseBtn = document.querySelector("#checkoutFormCloseBtn")
-
-checkoutFormOpenBtn.addEventListener("click", (e) => {
-	e.preventDefault()
-	checkoutForm.classList.add("is-active")
-})
-
-checkoutFormCloseBtn.addEventListener("click", (e) => {
-	e.preventDefault()
-	checkoutForm.classList.remove("is-active")
-})
